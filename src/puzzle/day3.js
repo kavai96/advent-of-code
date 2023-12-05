6171875481ec1f60d497b217f1399008d3e96b13
aensys-akavai <arnold.kavai@aensys.hu>
2023-12-05 18:02:16 +0100

    day 3 part 2 half solution

--- src/puzzle/day3.js
+++ src/puzzle/day3.js
@@ -76,6 +76,127 @@ const generateNeighbourCoordinates = (i, j) => {
   ];
 };
 
+async function day3part2() {
+  const filePath = "./src/input/input3.txt";
+  const fileContent = await readFileAndCreateArray(filePath);
+
+  const test = fileContent.reduce((acc, current) => {
+    const characters = current.split("").filter((char) => char !== "\r");
+    acc.push(characters);
+    return acc;
+  }, []);
+
+  let sum = 0;
+
+  test.forEach((row, i) => {
+    row.forEach((char, j) => {
+      if (char !== "*") return;
+
+      const neighbours = countNeighbours(i, j, test);
+
+      if (neighbours.length !== 2) return;
+
+      const [ratio1, ratio2] = neighbours;
+
+      sum += ratio1 * ratio2;
+
+      console.log("neighbours", neighbours);
+    });
+  });
+
+  console.log("res", sum);
+}
+
+const generateNeighbourCoordinates2 = (i, j) => {
+  return [
+    { i: i - 1, j: j - 3 },
+    { i: i - 1, j: j - 2 },
+    { i: i - 1, j: j - 1 },
+    { i: i - 1, j },
+    { i: i - 1, j: j + 1 },
+    { i: i - 1, j: j + 2 },
+    { i: i - 1, j: j + 3 },
+
+    { i, j: j - 3 },
+    { i, j: j - 2 },
+    { i, j: j - 1 },
+    { i, j },
+    { i, j: j + 1 },
+    { i, j: j + 2 },
+    { i, j: j + 3 },
+
+    { i: i + 1, j: j - 3 },
+    { i: i + 1, j: j - 2 },
+    { i: i + 1, j: j - 1 },
+    { i: i + 1, j },
+    { i: i + 1, j: j + 1 },
+    { i: i + 1, j: j + 2 },
+    { i: i + 1, j: j + 3 },
+  ];
+};
+
+const countNeighbours = (i, j, content) => {
+  const coordinates = generateNeighbourCoordinates2(i, j);
+
+  let valid = false;
+
+  let tempNumber = "";
+
+  return coordinates.reduce((acc, curr) => {
+    const first = content[curr.i];
+    if (!first) return acc;
+
+    const field = first[curr.j];
+
+    if (!field) return acc;
+
+    if (!isNumber(field) && tempNumber.length > 0) {
+      if (valid) {
+        acc.push(tempNumber);
+      }
+
+      tempNumber = "";
+      valid = false;
+
+      return acc;
+    }
+
+    if (curr.j === 3 && tempNumber.length > 0) {
+      if (valid) {
+        acc.push(tempNumber + field);
+      }
+
+      tempNumber = "";
+      valid = false;
+
+      return acc;
+    }
+
+    if (!isNumber(field)) return acc;
+
+    tempNumber += field;
+
+    if (valid) return acc;
+
+    valid = checkNeighbours2(curr.i, curr.j, content);
+
+    return acc;
+  }, []);
+};
+
+const checkNeighbours2 = (i, j, content) => {
+  const coordinates = generateNeighbourCoordinates(i, j);
+
+  return coordinates.some((cr) => {
+    const first = content[cr.i];
+    if (!first) return false;
+
+    const field = first[cr.j];
+
+    return field && !isNumber(field) && field === "*";
+  });
+};
+
 const checkNeighbours = (i, j, content) => {
   const coordinates = generateNeighbourCoordinates(i, j);
 
@@ -89,4 +210,4 @@ const checkNeighbours = (i, j, content) => {
   });
 };
 
-module.exports = { day3part1 };
+module.exports = { day3part1, day3part2 };
--- src/solve-puzzle.js
+++ src/solve-puzzle.js
@@ -1,13 +1,13 @@
 const { day1 } = require("./puzzle/day1");
 const { day2Part1, day2Part2 } = require("./puzzle/day2");
-const { day3part1 } = require("./puzzle/day3");
+const { day3part1, day3part2 } = require("./puzzle/day3");
 const { day4Part1, day4Part2 } = require("./puzzle/day4");
 
 async function run() {
   //await day1();
   //await day2Part1();
   //await day4Part2();
-  await day3part1();
+  await day3part2();
   process.exit(0);
 }
 
