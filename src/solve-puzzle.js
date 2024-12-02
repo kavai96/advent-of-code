3c720f680aafd7df782680377a6c7959b9197d68
aensys-akavai <arnold.kavai@aensys.hu>
2024-12-02 13:45:52 +0100

    2024 day2 solutions

--- src/solve-puzzle.js
+++ src/solve-puzzle.js
@@ -1,13 +1,8 @@
-//const { day1 } = require("./puzzle/day1");
-const { day2Part1, day2Part2 } = require("./puzzle/day2");
-const { day3part1, day3part2 } = require("./puzzle/day3");
-const { day4Part1, day4Part2 } = require("./puzzle/day4");
-const { day5part1 } = require("./puzzle/day5");
-
 const { day1 } = require("./puzzle/2024/day1");
+const { day2 } = require("./puzzle/2024/day2");
 
 async function run() {
-  await day1();
+  await day2();
   process.exit(0);
 }
 
